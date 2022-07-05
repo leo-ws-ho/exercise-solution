abb23661479c0afd5b41f8f36d0ac0288e9aae60
Leo Ho <leo.ho@mindera.com>
2022-07-05 18:28:10 +0100

    Add Solution to Question 1 of the Exercise.
    This is a multi-line commit message for
    our test.

---
+++ solution1.js
@@ -0,0 +1,7 @@
+const teachingTeam = [ 'Leo', 'Kirti', 'PJ', 'Gemma', 'James', 'Hello', 'P' ];
+
+for (member of teachingTeam) {
+    if (member.includes('o')) {
+        console.log(member);
+    }
+}
