915e229d948757a460f66a0bfcb5c8d2705493a6
Leo Ho <leo.ho@mindera.com>
2022-07-05 18:38:42 +0100

    Solution 2 added.

---
+++ solution2.js
@@ -0,0 +1,7 @@
+const teachingTeam = [ 'Leo', 'Kirti', 'PJ', 'Gemma', 'James', 'Hello', 'P' ];
+
+for (let i = 0; i < teachingTeam.length; i++) {
+    teachingTeam[i] = teachingTeam[i].toUpperCase();
+}
+
+console.log(teachingTeam);
